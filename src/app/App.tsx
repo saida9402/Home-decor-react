740d93554fd89bb7f438589a2f6037c144390d9a
saida9402 <saidahaymatova@gmail.com>
2026-07-22 14:46:37 +0900

    fix: modify App routing and ContextProvider components

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,35 +1,33 @@
 import React, { useState } from "react";
-import { Route, Switch, useLocation } from "react-router-dom";
-import  HomePage  from "./screens/homePage";
-import  ProductsPage  from "./screens/productsPage";
-import  OrdersPage  from "./screens/ordersPage";
-import  UserPage from "./screens/userPage";
-import  HomeNavbar  from "./components/headers/HomeNavbar";
-import  OtherNavbar  from "./components/headers/OtherNavbar";
-import  Footer from "./components/footer";
-import  HelpPage  from "./screens/helpPage";
+import { Switch, Route, useLocation } from "react-router-dom";
+
+import HomePage from "./screens/homePage";
+import ProductsPage from "./screens/productsPage";
+import OrdersPage from "./screens/ordersPage";
+import UserPage from "./screens/userPage";
+import HomeNavbar from "./components/headers/HomeNavbar";
+import OtherNavbar from "./components/headers/OtherNavbar";
+import Footer from "./components/footer";
+import HelpPage from "./screens/helpPage";
 import useBasket from "./hooks/useBasket";
 import AuthenticationModal from "./components/auth";
 import { sweetErrorHandling, sweetTopSuccessAlert } from "../lib/sweetAlert";
 import { Messages } from "../lib/config";
 import MemberService from "./services/MemberService";
 import { useGlobals } from "./hooks/useGlobals";
+
 import "../css/app.css";
 import "../css/navbar.css";
 import "../css/footer.css";
 
 function App() {
-  const { setAuthMember} = useGlobals();
+  const { setAuthMember } = useGlobals();
   const location = useLocation();
-  const {cartItems, onAdd, onRemove, onDelete, onDeleteAll} = useBasket();
-  const [signupOpen, setSignupOpen] = useState<boolean>(false);
-  const [loginOpen, setLoginOpen] = useState<boolean>(false);
+  const { cartItems, onAdd, onRemove, onDelete, onDeleteAll } = useBasket();
+
+  const [signupOpen, setSignupOpen] = useState(false);
+  const [loginOpen, setLoginOpen] = useState(false);
   const [anchorEl, setAnchorEl] = useState<HTMLElement | null>(null);
-  
-  
-  
-  
-/** HANDLERS **/
 
   const handleSignupClose = () => setSignupOpen(false);
   const handleLoginClose = () => setLoginOpen(false);
@@ -39,83 +37,69 @@ function App() {
   };
 
   const handleCloseLogout = () => setAnchorEl(null);
+
   const handleLogoutRequest = async () => {
     try {
       const member = new MemberService();
       await member.logout();
-
       await sweetTopSuccessAlert("success", 800);
       setAuthMember(null);
     } catch (err) {
-      console.log(err);
-      sweetErrorHandling(Messages.error1)
+      console.error(err);
+      sweetErrorHandling(Messages.error1);
     }
   };
 
   return (
-    <>   
-        {location.pathname === "/" ? (
-          <HomeNavbar
-           cartItems={cartItems}
-           onAdd={onAdd}
-           onRemove={onRemove}
-           onDelete={onDelete}
-           onDeleteAll={onDeleteAll}
-           setSignupOpen={setSignupOpen}
-           setLoginOpen={setLoginOpen}
-           anchorEl={anchorEl}
-           handleLogoutClick={handleLogoutClick}
-           handleCloseLogout={handleCloseLogout}
-           handleLogoutRequest={handleLogoutRequest}
-           /> 
-        ): (
+    <>
+      {location.pathname === "/" ? (
+        <HomeNavbar
+          cartItems={cartItems}
+          onAdd={onAdd}
+          onRemove={onRemove}
+          onDelete={onDelete}
+          onDeleteAll={onDeleteAll}
+          setSignupOpen={setSignupOpen}
+          setLoginOpen={setLoginOpen}
+          anchorEl={anchorEl}
+          handleLogoutClick={handleLogoutClick}
+          handleCloseLogout={handleCloseLogout}
+          handleLogoutRequest={handleLogoutRequest}
+        />
+      ) : (
         <OtherNavbar
-         cartItems={cartItems}
-         onAdd={onAdd}
-         onRemove={onRemove}
-         onDelete={onDelete}
-         onDeleteAll={onDeleteAll}
-         setSignupOpen={setSignupOpen}
-         setLoginOpen={setLoginOpen}
-         anchorEl={anchorEl}
-         handleLogoutClick={handleLogoutClick}
-         handleCloseLogout={handleCloseLogout}
-         handleLogoutRequest={handleLogoutRequest}
-         
-          />
-        )}
-        <Switch>
-          <Route path="/products">
-            <ProductsPage onAdd={onAdd} />
-          </Route>
-          <Route path="/orders">
-            <OrdersPage />
-          </Route>
-          <Route path="/member-page">
-            <UserPage />
-          </Route>
-          <Route path="/help">
-            <HelpPage />
-          </Route>
-          <Route path="/">
-           <HomePage />
-          </Route>
-        </Switch>
-        <Footer />
+          cartItems={cartItems}
+          onAdd={onAdd}
+          onRemove={onRemove}
+          onDelete={onDelete}
+          onDeleteAll={onDeleteAll}
+          setSignupOpen={setSignupOpen}
+          setLoginOpen={setLoginOpen}
+          anchorEl={anchorEl}
+          handleLogoutClick={handleLogoutClick}
+          handleCloseLogout={handleCloseLogout}
+          handleLogoutRequest={handleLogoutRequest}
+        />
+      )}
 
-        <AuthenticationModal 
+      <Switch>
+        <Route path="/products" component={ProductsPage} />
+        <Route path="/orders" component={OrdersPage} />
+        <Route path="/member-page" component={UserPage} />
+        <Route path="/help" component={HelpPage} />
+        <Route path="/" component={HomePage} />
+      </Switch>
+
+      <Footer />
+
+      <AuthenticationModal
         signupOpen={signupOpen}
         loginOpen={loginOpen}
         handleLoginClose={handleLoginClose}
         handleSignupClose={handleSignupClose}
-        />
-      </>
+      />
+    </>
   );
 }
 
 export default App;
-
-
-
-
-
